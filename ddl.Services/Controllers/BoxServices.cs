7075f6772135caf86d267333617be4ff4a36cb5c
lemanzeynalova <mi7NJBDX9@code.edu.az>
2023-11-12 20:01:39 +0400

    create-basket-services

---
+++ ddl.Services/Controllers/BoxServices.cs
@@ -0,0 +1,33 @@
+﻿using ddl.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddl.Services.Controllers
+{
+    public static class BoxServices
+    {
+        public static void AddBox(Box box)
+        {
+            DataBase.boxes.Add(box);
+        }
+        public static bool NullControl(Box box)
+        {
+            if(box!=null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static Box GetById(int id)
+        {
+            return DataBase.boxes.FirstOrDefault(x => x.Id == id);
+        }
+        public static void RemoveBox(int id)
+        {
+            DataBase.boxes.Remove(GetById(id));
+        }
+    }
+}
